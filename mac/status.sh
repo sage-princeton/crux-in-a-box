fcf8661d6707f8e5afbe5559ebf7d33a563c8796
Andrew Schwartz <andrew@aschwartz.me>
2026-06-04 16:39:55 -0600

    chore: rm mac

--- mac/status.sh
+++
@@ -1,6 +0,0 @@
-# TODO: confirm gh logged in
-# TODO: confirm gog logged in
-# TODO: confirm aws logged in
-# TODO: confirm telemetry is configured in the config
-# TODO: confirm latest monitoring update and count tarfiles / images in the output dir
-# TODO: confirm that monitor.sh is running via ps
